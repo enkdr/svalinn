480672c3472d6689b0bba89c193bd5204ec12020
Dev Enkoder <dev@enkoder.com.au>
2019-03-26 02:57:46 +1100

    start building react ui toggle log in

--- ui/src/App.js
+++ ui/src/App.js
@@ -1,25 +1,41 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import 'bootstrap/dist/css/bootstrap.css';
+import './app.css';
+import Login from './components/Login';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      visibility: true
+    };
+    this.toggleVisibility = this.toggleVisibility.bind(this);
+  }
+
+  toggleVisibility() {
+    this.setState({
+      visibility: !this.state.visibility
+    });
+  }
+
   render() {
+    let landing;
+    if (this.state.visibility) {
+      landing = <h1>S V A L I N N</h1>;
+    } else {
+      landing = <Login />;
+    }
+
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div className='app'>
+        <small
+          className='app-login-link font-weight-light'
+          onClick={this.toggleVisibility}
+        >
+          Login
+        </small>
+        <header className='app-header'>{landing}</header>
       </div>
     );
   }
